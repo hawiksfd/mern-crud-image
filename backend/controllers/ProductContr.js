cbc610f86b3923a917e01baa473a27553691cc02
hawik <hawiksyaef@gmail.com>
2022-08-09 15:26:14 +0700

    menambahkan controller get

---
+++ backend/controllers/ProductContr.js
@@ -0,0 +1,36 @@
+import Product from "../models/ProductModel.js";
+import path from 'path';
+
+export const getProduct = async (req, res) => {
+    try {
+        const response = await Product.findAll();
+        res.json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const getProductById = async (req, res) => {
+    try {
+        const response = await Product.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const saveProduct = (req, res) => {
+
+}
+
+export const updateProduct = (req, res) => {
+
+}
+
+export const deleteProduct = (req, res) => {
+
+}
